6978b76f4124f81d6de72c33fe4a9bc39cc8c614
Michele Vitulli <github@jesap.it>
2023-03-28 12:25:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 import streamlit as st
 
-
-st.write('Michele Vitulli nacque il 4 luglio del 1960 a Napoli, da una famiglia di militari. Fin da giovane, dimostrò un grande interesse per la storia e la strategia militare, che lo portò a frequentare l\'Accademia Militare di Modena. Durante la sua carriera militare, Michele Vitulli dimostrò una grande abilità tattica, distinguendosi in numerose missioni delicate e pericolose. Grazie alla sua determinazione e alla sua intelligenza, ottenne rapidamente la promozione a generale, diventando uno dei più giovani e promettenti comandanti dell\'esercito italiano. La sua fama di stratega brillante e implacabile lo portò a essere scelto per comandare una missione di pace in un paese africano dilaniato dalla guerra civile. Grazie alla sua abilità nel negoziare con le diverse fazioni in conflitto e nel coordinare le forze internazionali presenti sul territorio, riuscì a riportare la pace e la stabilità in quella regione. La sua abilità nel campo militare gli valse numerosi riconoscimenti, tra cui la Croce al Merito di Guerra e l\'Ordine al Merito della Repubblica Italiana. Tuttavia, Michele Vitulli non si considerava solo un uomo di guerra, ma anche un uomo di pace e di cultura.')
-
-st.write('Durante la sua vita, infatti, si dedicò anche alla promozione della cultura e dell\'arte, finanziando numerose iniziative volte a diffondere la conoscenza tra la gente comune. In particolare, fondò una scuola di arte e musica a Napoli, che divenne presto un punto di riferimento per i giovani talenti della città.')
-
-st.write('Michele Vitulli morì il 23 marzo del 2023, a causa di una malattia incurabile. La sua morte fu molto sentita non solo nel mondo militare, ma anche tra i numerosi amici e conoscenti che aveva fatto lungo il corso della sua vita. La sua figura resterà sempre un esempio di dedizione, coraggio e intelligenza al servizio della propria nazione e dell\'umanità intera.')
-
+sito = st.textinput('Inserisci il nome del sito del quale vuoi avere informazioni')
