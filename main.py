7098f118cb8e78f1973e67b84f3f2200799ef397
Michele Vitulli <github@jesap.it>
2023-03-28 12:30:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 sito = st.text_input('Inserisci il nome del sito del quale vuoi avere informazioni (es. jesap.it)')
 
-completo = "https/\/:" + sito
+completo = "https\/\/:" + sito
 if sito:
     st.write(completo)
